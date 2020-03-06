99fba287debed08dafe7b7725de45f395fabea5e
Anna Butrimenko <annettt@gmail.com>
2020-03-06 09:36:30 +0200

    Fixed eslint warnings

---
+++ client/src/components/Recipe-item/recipe-item.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import {Container, Divider} from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+import DialogModal from '../Dialog-Modal/dialog-modal';
+import Date from '../Date/date';
+
+const RecipeItem = ({ recipe, onRemoveRecipe }) => {
+  const {
+    title,
+    description,
+    date
+  } = recipe;
+  const [isDialogOpen, setDialogOpen] = useState(false);
+  const handleDialogOpen = () => {
+    setDialogOpen(true)
+  };
+  const onCloseDialogWithStatus = (status) => {
+    if (status) {
+      onRemoveRecipe(recipe._id)
+    }
+    setDialogOpen(false)
+  };
+  const renderDescription = description.split('\n').map((item) => {
+    return (
+      <p>
+        {item}
+      </p>
+    )
+  });
+  return (
+    <Container>
+      <h1>{title}</h1>
+      <Date date={date} />
+      <div>{renderDescription}</div>
+      <DeleteIcon color="secondary" onClick={() => { handleDialogOpen() }} />
+      <DialogModal onCloseDialogWithStatus={onCloseDialogWithStatus} isOpen={isDialogOpen} />
+      <Divider />
+    </Container>
+  )
+};
+
+export default RecipeItem;
